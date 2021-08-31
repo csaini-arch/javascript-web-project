ea3604eec7398a7d263d6020cb4fad32d0d1e2e1
csaini <csaini@csustan.edu>
2021-08-30 22:18:46 -0700

    second draft

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+//  By Daniel Shiffman
+//  https://youtu.be/E4RyStef-gY
 function setup() {
-createCanvas(windowWidth,windowHeight);
-angleMode(DEGREES);
+  bg = loadImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpAI0aYIm7gt-vgc5KwjiN2UsmB6YhY5_Yb3-VZzKAkAehP8kNbNPUgHSjkSK9n6vnCvY&usqp=CAU');
+  bg.resize(windowWidth, windowHeight);
+  createCanvas(windowWidth,windowHeight);
+  angleMode(DEGREES);
 }
 
 function draw() {
-  background(0);
-  translate(200, 200);
+  background(bg);
+  translate(770, 375);
   rotate(-90);
-
+    
   let hr = hour();
   let mn = minute();
   let sc = second();
@@ -16,6 +20,7 @@ function draw() {
   stroke(255, 100, 150);
   noFill();
   let secondAngle = map(sc, 0, 60, 0, 360);
+  
 
   stroke(150, 100, 255);
   let minuteAngle = map(mn, 0, 60, 0, 360);
@@ -23,27 +28,26 @@ function draw() {
   stroke(150, 255, 100);
   let hourAngle = map(hr % 12, 0, 12, 0, 360);
 
-  push();
+  //push();
   rotate(secondAngle);
   stroke(255, 100, 150);
-  line(0, 0, 100, 0);
-  pop();
+  line(0, 0, 190, 0);
+  //pop();
 
   push();
   rotate(minuteAngle);
   stroke(150, 100, 255);
-  line(0, 0, 75, 0);
+  line(0, 0, 150, 0);
   pop();
 
   push();
   rotate(hourAngle);
   stroke(150, 255, 100);
-  line(0, 0, 50, 0);
+  line(0, 0, 90, 50);
   pop();
+
 
   stroke(255);
   point(0, 0);
-
-  circle(0, 0, 300);
 
 }
