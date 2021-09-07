2d894d0297b516747b1c9cb892f2d714a1e672b3
csaini <csaini@csustan.edu>
2021-09-07 02:40:43 -0700

    final-draft

--- app.js
+++ app.js
@@ -1,109 +1,91 @@
-//  By Daniel Shiffman
-//  https://youtu.be/E4RyStef-gY
+// By Hakan Yorganci
+// https://gist.github.com/hakanovski/4433e5f4c42c118f3dccd55c5d4199e0
 
+// Variables
+let clockRadius = 160;
+let numPoints = 60;
+let secondLength = clockRadius * 1.2;
+let minuteLength = clockRadius * 0.85;
+let hourLength = clockRadius * 0.65;
+let secondWidth = 8;
+let minuteWidth = 8;
+let hourWidth = 8;
+let centerX;
+let centerY;
+let clockHeight = clockRadius * 3;
+let clockWidth = clockRadius * 3;
+let lineY1 = 240;
+let lineY2 = 220;
+
+
+// Creating canvas
 function setup() {
   bg = loadImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpAI0aYIm7gt-vgc5KwjiN2UsmB6YhY5_Yb3-VZzKAkAehP8kNbNPUgHSjkSK9n6vnCvY&usqp=CAU');
   bg.resize(windowWidth, windowHeight);
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
+  centerX = windowWidth / 2;
+  centerY = windowHeight / 2;
 }
 
+// Clock face
+function clockSurface() {
+  noFill();
+  stroke(0, 0, 0);
+  strokeWeight(9);
+  ellipse(centerX, centerY, clockHeight, clockWidth);
+
+  translate(centerX, centerY);
+
+  // Clock tick marks
+  for (let i = 0; i < 12; i++) {
+    strokeWeight(4);
+    line(0, lineY1, 0, lineY2);
+    rotate(30);
+  }
+}
+
+// Drawing the functions on canvas
 function draw() {
   background(bg);
-  translate(755, 360);
-  rotate(-90);
 
-  // Clock Hands
-    
-  let hr = hour();
-  let mn = minute();
-  let sc = second();
+  clockSurface();
 
-  strokeWeight(8);
-  stroke(255, 100, 150);
-  noFill();
-  let secondAngle = map(sc, 0, 60, 0, 360); // mapping the seconds hand
+  translate(-centerX, -centerY);
 
-  stroke(150, 100, 255);
-  let minuteAngle = map(mn, 0, 60, 0, 360); // mapping the minutes hand
-
-  stroke(150, 255, 100);
-  let hourAngle = map(hr % 12, 0, 12, 0, 360); // mapping the hours hand
-
-  push(); // moving the seconds hand
-  rotate(secondAngle);
+  // Moving the Second Hand
+  let  sec = map(second(), 0, 60, 0, 360) - 90;
   stroke(44, 78, 202);
-  line(0, 0, 180, 0);
-  pop();
+  strokeWeight(secondWidth)
+  line(
+    centerX,
+    centerY,
+    centerX + secondLength * cos(sec),
+    centerY + secondLength * sin(sec)
+  );
 
-  push(); // moving the minutes hand
-  rotate(minuteAngle);
+
+  // Moving the Minute Hand
+  let  min = map(minute(), 0, 60, 0, 360) - 90;
   stroke(198, 26, 190);
-  line(0, 0, 140, 0);
-  pop();
+  strokeWeight(minuteWidth);
+  line(
+    centerX,
+    centerY,
+    centerX + minuteLength * cos(min),
+    centerY + minuteLength * sin(min)
+  );
 
-  push(); // moving the hours hand
-  rotate(hourAngle);
+
+  // Moving the Hour Hand
+  let  hr = map(hour(), 0, 12, 0, 360) - 90;
   stroke(238, 0, 83);
-  line(0, 0, 90, 0);
-  pop();
+  strokeWeight(hourWidth);
+  line(
+    centerX,
+    centerY,
+    centerX + hourLength * cos(hr),
+    centerY + hourLength * sin(hr)
+  );
 
-  stroke(255); // point in the center of the clock
-  point(0, 0);
-
-  stroke(0, 0, 0); // clock circle
-  ellipse(0, 0, 550, 550);
-  strokeWeight(4);
-
-  textSize(50); // number 12
-  rotate(90);
-  text('12', -20, -210);
-
-  textSize(50); // number 3
-  rotate(90);
-  text('3', -15, -210);
-
-  textSize(50); // number 6
-  rotate(90);
-  text('9', -15, -210);
-
-  textSize(50); // number 9
-  rotate(90);
-  text('9', -15, -210);
-
-  textSize(50); // number 1
-  rotate(122);
-  text('1', -15, -210);
-
-  textSize(50); // number 2
-  rotate(27);
-  text('2', -15, -210);
-
-  textSize(50); // number 4
-  rotate(60);
-  text('4', -15, -210);
-
-  textSize(50); // number 5
-  rotate(30);
-  text('5', -15, -210);
-
-  textSize(50); // number 7
-  rotate(65);
-  text('7', -15, -210);
-
-  textSize(50); // number 8
-  rotate(27);
-  text('8', -15, -210);
-
-  textSize(50); // number 10
-  rotate(55);
-  text('10', -15, -210);
-
-  textSize(50); // number 11
-  rotate(30);
-  text('11', -15, -210);
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
 }
